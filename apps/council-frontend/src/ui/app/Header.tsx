0645e40925553c8110e03f4c5934835a254dd481
Zachary Rudebeck <zachary.rudebeck@gmail.com>
2022-10-01 15:37:35 -0500

    removed gas indicator

--- apps/council-frontend/src/ui/app/Header.tsx
+++ apps/council-frontend/src/ui/app/Header.tsx
@@ -2,22 +2,11 @@ import React, { ReactElement } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { t } from "ttag";
 import { WalletProfileButton } from "src/ui/wallet/ConnectWalletButton";
-import { useGasPrice } from "src/ui/ethereum/useGasPrice";
 import ElementUrl from "src/urls";
-import GasIcon from "src/ui/base/svg/GasIcon";
-import { useDeposited } from "src/ui/base/lockingVault/useDeposited";
-import {
-  ElementIconCircle,
-  IconSize,
-} from "src/ui/base/ElementIconCircle/ElementIconCircle";
-import Tooltip from "src/ui/base/Tooltip/Tooltip";
-import { TooltipDefinition } from "src/ui/voting/tooltipDefinitions";
 import { Provider } from "@ethersproject/providers";
 import ExternalLink from "src/ui/base/ExternalLink/ExternalLink";
 import { Divider } from "src/ui/base/Divider";
 import H2 from "src/ui/base/H2/H2";
-
-const GAS_URL = "https://www.etherchain.org/tools/gasnow";
 
 interface HeaderProps {
   title?: string;
@@ -26,9 +15,6 @@ interface HeaderProps {
 function Header(props: HeaderProps): ReactElement {
   const { title } = props;
   const { account, active, library } = useWeb3React<Provider>();
-  const { data: gasPrice } = useGasPrice();
-  const amountDeposited = useDeposited(account) || "0";
-  const formattedAmountDeposited = parseFloat(amountDeposited).toFixed(2);
 
   return (
     <div className="flex w-full flex-col">
@@ -46,26 +32,6 @@ function Header(props: HeaderProps): ReactElement {
                   className="text-fiatWhite"
                 />
               </span>
-
-              <ExternalLink
-                href={GAS_URL}
-                className="text-fiatWhite mr-8 flex items-center"
-                showIcon={false}
-              >
-                <GasIcon className="fill-fiatLavender h-5 w-5" />
-                <span className="font-bold">
-                  {gasPrice?.recommendedBaseFee || 0.0}
-                </span>
-              </ExternalLink>
-
-                {/*
-              <span className="text-fiatWhite mr-8 flex items-center gap-2 font-bold">
-                <ElementIconCircle size={IconSize.MEDIUM} />
-                  {formattedAmountDeposited}
-                  <span className="hidden lg:inline"> FDT</span>
-                </span>
-              </span>
-                */}
             </div>
           ) : null}
 
